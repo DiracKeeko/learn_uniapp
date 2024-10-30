c6c51aee4b5cf03004de344124bfe026311641cf
keeko <787018661@qq.com>
2024-10-30 20:48:31 +0800

    [chore] view标签属性

---
+++ uniappV3Demo01/pages/learnDemo01/learnDemo01.vue
@@ -0,0 +1,33 @@
+<template>
+	<view class="box" hover-class="box-hover" hover-start-time="10" hover-stay-time="50">
+		view布局标签
+		<view class="inner" hover-class="inner-hover" hover-stop-propagation>内部元素</view>
+	</view>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+	.box {
+		width: 200px;
+		height: 200px;
+		background-color: #ccc;
+	}
+
+	.inner {
+		width: 150px;
+		height: 150px;
+		background-color: green;
+	}
+
+	.box-hover {
+		width: 300px;
+		background-color: orange;
+	}
+
+	.inner-hover {
+		background-color: greenyellow;
+	}
+</style>
