e76d9586d76ed0a15326c267a31cc8ea89035429
keeko <787018661@qq.com>
2024-11-02 13:43:47 +0800

    <text> 标签 文本可选 空格大小

--- uniappV3Demo01/pages/learnDemo01/learnDemo01.vue
+++ uniappV3Demo01/pages/learnDemo01/learnDemo01.vue
@@ -3,6 +3,20 @@
 		view布局标签
 		<view class="inner" hover-class="inner-hover" hover-stop-propagation>内部元素</view>
 	</view>
+	
+	<view>view标签中的文字不能复制</view>
+	
+	<view>---------</view>
+	
+	<text selectable>text标签+文本可选</text>
+	
+	<br>
+	<text space="nbsp">text标签        连续空格</text>
+	<br>
+	<text space="ensp">text标签        连续空格</text>
+	<br>
+	<text space="emsp">text标签        连续空格</text>
+	
 </template>
 
 <script setup>
