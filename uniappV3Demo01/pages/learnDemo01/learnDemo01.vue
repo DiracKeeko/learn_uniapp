2089a436d4f292adb8561e14a45fa2d8234817d5
keeko <787018661@qq.com>
2024-11-05 20:11:22 +0800

    [feat] scroll-view 上下滑动、左右滑动

--- uniappV3Demo01/pages/learnDemo01/learnDemo01.vue
+++ uniappV3Demo01/pages/learnDemo01/learnDemo01.vue
@@ -3,20 +3,47 @@
 		view布局标签
 		<view class="inner" hover-class="inner-hover" hover-stop-propagation>内部元素</view>
 	</view>
-	
+
 	<view>view标签中的文字不能复制</view>
-	
+
 	<view>---------</view>
-	
+
 	<text selectable>text标签+文本可选</text>
-	
+
 	<br>
-	<text space="nbsp">text标签        连续空格</text>
+	<text space="nbsp">text标签 连续空格</text>
 	<br>
-	<text space="ensp">text标签        连续空格</text>
+	<text space="ensp">text标签 连续空格</text>
 	<br>
-	<text space="emsp">text标签        连续空格</text>
-	
+	<text space="emsp">text标签 连续空格</text>
+
+	<br>
+	<scroll-view scroll-y="true" scroll-x class="first-scroll-view">
+		<view class="inner-box">scroll的子元素</view>
+		<view class="inner-box">scroll的子元素</view>
+		<view class="inner-box">scroll的子元素</view>
+		<view class="inner-box">scroll的子元素</view>
+		<view class="inner-box">scroll的子元素</view>
+		<view class="inner-box">scroll的子元素</view>
+		<view class="inner-box">scroll的子元素</view>
+		<view class="inner-box">scroll的子元素</view>
+		<view class="inner-box">scroll的子元素</view>
+		<view class="inner-box">scroll的子元素</view>
+		<view class="inner-box">scroll的子元素</view>
+		<view class="inner-box">scroll的子元素</view>
+		<view class="inner-box">scroll的子元素</view>
+		<view class="inner-box">scroll的子元素</view>
+		<view class="inner-box">scroll的子元素</view>
+		<view class="inner-box">scroll的子元素</view>
+		<view class="inner-box">scroll的子元素</view>
+		<view class="inner-box">scroll的子元素</view>
+		<view class="inner-box">scroll的子元素</view>
+		<view class="inner-box">scroll的子元素</view>
+		<view class="inner-box">scroll的子元素</view>
+		<view class="inner-box">scroll的子元素</view>
+		<view class="inner-box">scroll的子元素</view>
+		<view class="inner-box">scroll的子元素</view>
+	</scroll-view>
 </template>
 
 <script setup>
@@ -43,5 +70,16 @@
 
 	.inner-hover {
 		background-color: greenyellow;
+	}
+
+	.first-scroll-view {
+		width: 50%;
+		height: 200px;
+		border: 1px solid red;
+		.inner-box {
+			width: 600px;
+			height: 50px;
+			border: 1px solid blue;
+		}
 	}
 </style>
