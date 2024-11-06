7e14851ec9f62f36bc2272703fbddf81faa74c3f
Keeko <787018661@qq.com>
2024-11-06 20:18:42 +0800

    [feat] swiper自动轮播 轮播时间、方向

--- uniappV3Demo01/pages/learnDemo02/learnDemo02.vue
+++ uniappV3Demo01/pages/learnDemo02/learnDemo02.vue
@@ -3,17 +3,32 @@
     <!-- swiper 轮播器 -->
     <!-- 指示点 indicator-dots, indicator-color + indicator-active-color -->
     <!-- 指示点 衔接 circular -->
+    <!-- 自动播放 autoplay 自动切换时间间隔 interval 单位是ms -->
     <swiper
       indicator-dots="true"
       indicator-color="blue"
       indicator-active-color="#c020f0"
       circular="true"
+      autoplay="true"
+      interval="1500"
     >
       <swiper-item>111</swiper-item>
       <swiper-item>222</swiper-item>
       <swiper-item>333</swiper-item>
       <swiper-item>444</swiper-item>
     </swiper>
+
+    <!-- vertical 纵向轮播，默认为false -->
+    <swiper
+      indicator-dots="true"
+      vertical="true"
+    >
+      <swiper-item>111</swiper-item>
+      <swiper-item>222</swiper-item>
+      <swiper-item>333</swiper-item>
+      <swiper-item>444</swiper-item>
+    </swiper>
+
   </view>
 </template>
 
