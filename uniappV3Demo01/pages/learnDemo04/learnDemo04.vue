0ef6d258869f598e59d0f0599de5ae1123d362fc
Keeko <787018661@qq.com>
2024-11-10 10:17:14 +0800

    [chore] uni-app中学习vue3基础(组合式语法)

---
+++ uniappV3Demo01/pages/learnDemo04/learnDemo04.vue
@@ -0,0 +1,21 @@
+<template>
+  <view>
+    {{ a }}
+  </view>
+  <view>
+    <span>当前时间戳: </span>
+    {{ Date.now() }}
+  </view>
+  <view>
+    {{ fn() }}
+  </view>
+</template>
+
+<script setup>
+const a = 6;
+function fn() {
+  return "在uni-app中学习vue3基础(组合式语法)";
+}
+</script>
+
+<style></style>
