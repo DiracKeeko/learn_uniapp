40837b1ff1ce1d559af9dfab22d1a761d4d5ebef
Keeko <787018661@qq.com>
2024-11-11 11:37:05 +0800

    [feat] image src变化，实现图片轮播

---
+++ uniappV3Demo01/pages/learnDemo05/learnDemo05.vue
@@ -0,0 +1,29 @@
+<template>
+	<view>
+		<image :src="picUrl"></image>
+	</view>
+</template>
+
+<script setup>
+	import {ref } from "vue";
+	
+	const urlArr = [
+		"../../static/pic1.png",
+		"../../static/pic2.png",
+		"../../static/pic4.jpg",
+	];
+	
+	const picUrl = ref(urlArr[0]);
+	
+	let i = 0;
+	setInterval(() => {
+		const len = urlArr.length;
+		i += 1;
+		picUrl.value = urlArr[i % len];
+	}, 1600)
+	
+</script>
+
+<style>
+	       
+</style>
