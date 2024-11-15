32a963db7a3cf51c2aaaf31363a2616f2e9b01db
Keeko <787018661@qq.com>
2024-11-15 11:10:20 +0800

    [chore] v-for

--- uniappV3Demo01/pages/learnDemo06/learnDemo06.vue
+++ uniappV3Demo01/pages/learnDemo06/learnDemo06.vue
@@ -11,6 +11,14 @@
     <template v-if="true">
       <view class="real-dom">真实的dom元素</view>
     </template>
+		
+		<br />
+		<view v-for="(item, index) in 5" :key="index">box {{ index }}</view>
+		<!-- template 上写 v-for template也不会变为真实的dom元素 -->
+    <!-- 
+      vue 2.x 版本中在一个元素上同时使用 v-if 和 v-for 时，v-for 优先。
+      vue 3.x 版本中在一个元素上同时使用 v-if 和 v-for 时，v-if 优先。
+     -->
   </view>
 </template>
 
