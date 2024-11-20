0945b3e04bb16e5c6009397e872771e65baadef6
Keeko <787018661@qq.com>
2024-11-20 16:38:53 +0800

    [feat] 复杂场景的computed使用

--- uniappV3Demo01/pages/learnDemo07/learnDemo07.vue
+++ uniappV3Demo01/pages/learnDemo07/learnDemo07.vue
@@ -1,22 +1,49 @@
 <template>
   <view class="out">
     <!-- 这里的key 如果是 index 勾选复选框，再删除，就会有问题 -->
-    <view v-for="(item, index) in productList" :key="item.id">
-      <checkbox></checkbox>
-      <text class="title">{{ item.name }}</text>
-      <text class="del" @click="handleDelete(index)">删除</text>
+    <checkbox-group @change="itemChange">
+      <view v-for="(item, index) in productList" :key="item.id">
+        <checkbox :value="item.id" :checked="item.checked"></checkbox>
+        <text class="title">{{ item.name }}</text>
+        <text class="price">{{ item.price }}</text>
+        <text class="del" @click="handleDelete(index)">删除</text>
+      </view>
+    </checkbox-group>
+
+    <view class="card">
+      <view class="text"
+        >选中{{ totalNumber }}个，总价：{{ totalPrice }}元</view
+      >
     </view>
+
+    {{ selectGroup }}
+    <view>---</view>
   </view>
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { ref, computed } from "vue";
 const productList = ref([
-  { id: 11, name: "小米" },
-  { id: 22, name: "华为" },
-  { id: 33, name: "oppo" },
-  { id: 44, name: "苹果" },
+  { id: "11", name: "小米", price: 3999 },
+  { id: "22", name: "华为", price: 8848 },
+  { id: "33", name: "oppo", price: 4299 },
+  { id: "44", name: "苹果", price: 8199 },
 ]);
+
+const selectGroup = ref([]);
+const totalNumber = computed(() => selectGroup.value.length);
+const totalPrice = computed(() => {
+  return productList.value
+    .filter((item) => item.checked)
+    .reduce((acc, cur) => acc + cur.price, 0);
+});
+
+function itemChange(e) {
+  selectGroup.value = e.detail.value;
+  productList.value.forEach((incomingData) => {
+    incomingData.checked = selectGroup.value.includes(incomingData.id);
+  });
+}
 
 function handleDelete(index) {
   productList.value.splice(index, 1);
@@ -25,9 +52,17 @@ function handleDelete(index) {
 
 <style lang="scss" scoped>
 .out {
+  .price {
+    margin: 0 0 0 10px;
+  }
   .del {
     margin: 0 0 0 15px;
     color: red;
+  }
+  .card {
+    margin-top: 30px;
+    border-top: 1px solid #eee;
+    padding: 10px 0;
   }
 }
 </style>
