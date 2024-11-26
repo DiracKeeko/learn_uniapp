94288b48f7b52adb96904e9eedaa0facc8d42f8b
Keeko <787018661@qq.com>
2024-11-26 16:58:28 +0800

    [chore] 对props中的内容进行加工展示

--- uniappDemo02/components/UserInfo/UserInfo.vue
+++ uniappDemo02/components/UserInfo/UserInfo.vue
@@ -1,19 +1,25 @@
 <template>
   <view class="user-info">
     <image :src="avatar" mode="" class="avatar"></image>
-    <view>{{ username }}</view>
+    <view>{{ handleName(username) }}</view>
   </view>
 </template>
 
 <script setup>
-defineProps(["username", "avatar"])
+const props = defineProps(["username", "avatar"])
 /* 
 // 如果需要控制台打印，或者是处理username等传入参数的内容，则需要用变量接住这个props  
 // const props = defineProps([...])
 const props = defineProps(["username", "avatar"])
 console.log("username->", props.username);
- */
+*/
 
+// ↓ 执行失败，username没有变化。 因为props是单向数据源，只读。
+// props.username += "@"; // Set operation on key "username" failed: target is readonly
+// 如果想对username做处理，可以用computed，也可以用function
+function handleName(name) {
+  return name + "@";
+}
 
 </script>
 
