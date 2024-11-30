e88f74165e2faf6be934f4fc83fcb907de8c35bf
Keeko <787018661@qq.com>
2024-11-30 15:58:56 +0800

    [feat] 匿名插槽 (默认插槽)

---
+++ uniappDemo02/components/kk-layout/kk-layout.vue
@@ -0,0 +1,30 @@
+<template>
+	<view class="layout">
+		<view class="header">
+			顶部区域
+		</view>
+		<view class="main">
+			<slot></slot>
+		</view>
+		<view class="footer">底部区域</view>
+	</view>
+</template>
+
+<script setup>
+</script>
+
+<style lang="scss" scoped>
+.layout{
+	.header{
+		height: 100px;
+		background: #cfcfcf;
+	}
+	.main{
+		min-height: 200px;		
+	}
+	.footer{
+		height: 120px;
+		background: orange;
+	}
+}
+</style>
---
+++ uniappDemo02/pages/demo2/demo2.vue
@@ -0,0 +1,9 @@
+<template>
+  <view class="">
+    <kk-layout> 默认插槽 - 匿名插槽</kk-layout>
+  </view>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
