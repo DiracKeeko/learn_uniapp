23e4172ebf7b98f4de05c9da5e392b7a155715df
Keeko <787018661@qq.com>
2024-12-01 10:18:14 +0800

    [feat] 子组件传参到父组件

---
+++ uniappDemo02/components/kk-child/kk-child.vue
@@ -0,0 +1,26 @@
+<template>
+	<view>
+		child子组件
+		<button @click="$emit('add', 123)">123</button>
+		<button @click="handleClick">456</button>
+		<input type="text" @input="handleInput" />
+	</view>
+</template>
+
+<script setup>
+// 不同于 vue2 中 this.$emit(), 在vue3中需要先定义emit
+// 定义 emit (vue3中 定义emit)
+const emit = defineEmits(["add", "changeInput"]);
+
+function handleClick() {
+	emit("add", 456);
+}
+
+function handleInput(e) {
+	emit("changeInput", e.detail.value);
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ uniappDemo02/pages/demo3/demo3.vue
@@ -0,0 +1,24 @@
+<template>
+	<view class="">
+		<kk-child @add="handleAdd" @changeInput="handleInput"></kk-child>
+		显示内容: {{ showText }}
+	</view>
+</template>
+
+<script setup>
+import { ref } from "vue";
+const showText = ref(111);
+
+function handleAdd(param) {
+	console.log("param->", param);
+	showText.value = param;
+}
+
+function handleInput(text) {
+	showText.value = text;
+}
+</script>
+
+<style lang="scss" scoped>
+	       
+</style>
