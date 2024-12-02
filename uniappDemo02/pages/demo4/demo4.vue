3343c145d3ff4f0c71e1a9c99d08d6ab48c4a4d0
Keeko <787018661@qq.com>
2024-12-02 11:21:38 +0800

    [chore] vue3中的onMounted周期 定义

---
+++ uniappDemo02/pages/demo4/demo4.vue
@@ -0,0 +1,22 @@
+<template>
+  <view class="">
+		<scroll-view :scroll-x="false" :scroll-y="true" class="scroll" ref="theScroll">
+      <view>1111</view>
+    </scroll-view>
+  </view>
+</template>
+
+<script setup>
+// vue3中的mounted生命周期
+import { ref, onMounted } from "vue";
+const theScroll = ref(null); // 常量名和scroll-view中的ref名保持一致，在mounted周期内会自动绑定
+
+console.log("setup-> scroll->", theScroll.value);
+
+onMounted(()=>{
+	console.log("onMounted-> scroll->", theScroll.value);
+})
+
+</script>
+
+<style lang="scss" scoped></style>
