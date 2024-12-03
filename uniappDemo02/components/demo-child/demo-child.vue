92b9831d027d6664eaad05053fb7823e951c36f8
Keeko <787018661@qq.com>
2024-12-03 10:29:21 +0800

    [feat] defineExpose 向外暴露组件的属性和方法 并使用

---
+++ uniappDemo02/components/demo-child/demo-child.vue
@@ -0,0 +1,25 @@
+<template>
+  <view class="out"> 子组件count值：{{ count }} </view>
+</template>
+
+<script setup>
+import { ref } from "vue";
+const count = ref(100);
+const updateCount = function () {
+  count.value++;
+};
+
+// defineExpose 向外暴露组件的属性和方法
+defineExpose({
+  count,
+  str: "hello",
+  updateCount,
+});
+</script>
+
+<style lang="scss" scoped>
+.out {
+  padding: 20px;
+  background: #eee;
+}
+</style>
---
+++ uniappDemo02/pages/demo5/demo5.vue
@@ -0,0 +1,25 @@
+<template>
+  <view class="">
+    <demo-child ref="child"></demo-child>
+    <view>---------</view>
+    <button @click="handleClick">父组件调用子组件方法</button>
+  </view>
+</template>
+
+<script setup>
+import { onMounted, ref } from "vue";
+const child = ref(null);
+
+console.log("child->", child.value);
+
+onMounted(() => {
+  console.log("child->", child.value);
+});
+
+function handleClick() {
+  child.value.updateCount();
+}
+
+</script>
+
+<style lang="scss" scoped></style>
