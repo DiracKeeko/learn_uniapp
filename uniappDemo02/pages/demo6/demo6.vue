c23e2d0daffff81c787abfac58b96fda5195b71c
keeko <787018661@qq.com>
2024-12-04 22:45:37 +0800

    [chore] uni-app的生命周期函数 onLoad

---
+++ uniappDemo02/pages/demo6/demo6.vue
@@ -0,0 +1,21 @@
+<template>
+	<view>
+		姓名: {{ name }}
+	</view>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import { onLoad } from "@dcloudio/uni-app";
+
+const name = ref("Tom");
+onLoad(() => {
+	console.log("onLoad");
+	name.value = "Jerry"
+})
+	
+</script>
+
+<style>
+	       
+</style>
