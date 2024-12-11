590463107213027608f9328ebabc3c138f18b3a7
Keeko <787018661@qq.com>
2024-12-11 20:04:03 +0800

    [feat] uniapp中的rpx单位  ui设计稿尺寸变更

---
+++ uniappDemo02/pages/demo8/demo8.vue
@@ -0,0 +1,29 @@
+<template>
+	<view class="layout">
+		<view class="box">这是box</view>
+		<view class="text">
+			欢迎使用
+		</view>
+	</view>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+// video36 讲解通过PS改变设计稿尺寸，以及在线设计网站改变图像尺寸
+.box{
+	// width: 60%;
+	// width: 40vw;
+	// width: 400px;
+	width: 375rpx; // rpx是响应式宽度的单位 在uniapp中 750rpx是100%宽度, 375rpx是50%宽度
+	height: 130rpx;
+	background: orange;
+	font-size: 28rpx;
+}
+.text{
+	font-size: 52rpx;
+	// font-size: 52px;
+}
+</style>
