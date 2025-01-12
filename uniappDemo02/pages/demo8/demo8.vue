148d59fbe271f8cfe9aebedeb9c3ebcfa2f12fee
Keeko <787018661@qq.com>
2025-01-12 11:27:15 +0800

    [feat] 页面滑动到底触发 onReachBottom  hooks

--- uniappDemo02/pages/demo8/demo8.vue
+++ uniappDemo02/pages/demo8/demo8.vue
@@ -2,10 +2,17 @@
   <view class="layout">
     <view class="box">这是box</view>
     <view class="text"> 欢迎使用 </view>
+    <view v-for="item in 100"> {{ item }} </view>
   </view>
 </template>
 
-<script setup></script>
+<script setup>
+import { onReachBottom } from "@dcloudio/uni-app";
+
+onReachBottom(() => {
+  console.log("到底了");
+});
+</script>
 
 <style lang="scss" scoped>
 // video36 讲解通过PS改变设计稿尺寸，以及在线设计网站改变图像尺寸
